2a3d453da9d23ff222b565f279e10dbf11bb3d82
James Henning <jamesniklas@hotmail.de>
2023-06-27 12:41:24 +0200

    Change size

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
         type: "odds-comparison",
         domain: "widget-trial.vercel.app/",
         selector: "oddspedia-widget-container",
-        width: "400%",
+        width: "400px",
         theme: "1",
         odds_type: "1",
         language: "en",
