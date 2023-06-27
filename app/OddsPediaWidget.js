2aae6ca5e5de6d580762b4d6d4b63cbdf797ab20
James Henning <jamesniklas@hotmail.de>
2023-06-27 12:24:12 +0200

    Try widget again

--- app/OddsPediaWidget.js
+++ app/OddsPediaWidget.js
@@ -1,10 +1,6 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 
 const OddspediaWidget = () => {
-  const [widgetLoaded, setWidgetLoaded] = useState(false);
-
   useEffect(() => {
     const script = document.createElement('script');
     script.innerHTML = `
@@ -14,7 +10,7 @@ const OddspediaWidget = () => {
         domain: "widget-trial.vercel.app/",
         selector: "oddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false",
         width: "0",
-        theme: "1",
+        theme: "0",
         odds_type: "1",
         language: "en",
         primary_color: "#283E5B",
@@ -27,36 +23,21 @@ const OddspediaWidget = () => {
         leagues: ""
       };
     `;
-
     document.body.appendChild(script);
 
-    const widgetScript = document.createElement('script');
-    widgetScript.src =
+    const initScript = document.createElement('script');
+    initScript.src =
       'https://widgets.oddspedia.com/js/widget/init.js?widgetId=oddspediaWidgetOddsComparisonPopularSportsLeagues';
-    widgetScript.async = true;
-    widgetScript.onload = () => {
-      setWidgetLoaded(true);
-    };
-
-    document.body.appendChild(widgetScript);
+    initScript.async = true;
+    document.body.appendChild(initScript);
 
     return () => {
-      // Cleanup scripts when the component is unmounted
       document.body.removeChild(script);
-      document.body.removeChild(widgetScript);
+      document.body.removeChild(initScript);
     };
   }, []);
 
-  if (!widgetLoaded) {
-    return null; // Render nothing until the widget is loaded
-  }
-
-  return (
-    <div
-      id="oddspedia-widget-odds-comparison-popular-false-sports-false-leagues-false"
-      className="oddspedia-widget"
-    ></div>
-  );
+  return <div id="oddspedia-widget-container"></div>;
 };
 
 export default OddspediaWidget;
